0136cd8ebc360245dbe407af54cc7efe895592c9
dangquocbao1905 <dangquocbao1bao@gmail.com>
2020-09-24 20:45:23 +0700

    hoc

--- main.c
+++ main.c
@@ -2,13 +2,6 @@
 
 void main()
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
 	printf("hoctiengmeokeu\r\n");
-=======
-	printf("hi\r\n");
->>>>>>> b8b7d275a7b606fe774d518879d348b7dde0c892
-=======
-	printf("xin chao\r\n");
->>>>>>> c4d9a1fc5173b8132c527b76ddae72d140bdb5e0
+
 }
